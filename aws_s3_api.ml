27f40bfd0c34fb0e26147ce70b0f1d176db33bbe
Martin Jambon <martin@mjambon.com>
2016-03-10 15:14:54 -0800

    Work in progress

---
+++ aws_s3_api.ml
@@ -0,0 +1,26 @@
+(*
+   Client for Amazon Machine Learning
+
+   http://docs.aws.amazon.com/machine-learning/latest/APIReference/Welcome.html
+*)
+
+(*
+   CreateDataSourceFromS3
+   GetDataSource
+   DeleteDataSource
+
+   CreateMLModel
+   GetMLModel
+   DeleteMLModel
+
+   CreateEvaluation
+   GetEvaluation
+   DeleteEvaluation
+
+   CreateBatchPrediction
+   GetBatchPrediction
+   DeleteBatchPrediction
+*)
+
+let create_data_source_from_s3 () =
+  ()
