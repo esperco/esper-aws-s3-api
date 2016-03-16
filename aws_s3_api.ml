370d7a91ce785760ca94ca1438b2371a6f5091cc
Martin Jambon <martin@mjambon.com>
2016-03-15 18:53:43 -0700

    Minimal interface to S3

--- aws_s3_api.ml
+++ aws_s3_api.ml
@@ -1,26 +1,92 @@
 (*
-   Client for Amazon Machine Learning
+   Client for Amazon Simple Storage Service
 
-   http://docs.aws.amazon.com/machine-learning/latest/APIReference/Welcome.html
+   (incomplete)
 *)
 
-(*
-   CreateDataSourceFromS3
-   GetDataSource
-   DeleteDataSource
+open Printf
+open Lwt
+open Log
 
-   CreateMLModel
-   GetMLModel
-   DeleteMLModel
+let service = "s3"
 
-   CreateEvaluation
-   GetEvaluation
-   DeleteEvaluation
+type param = {
+  access_key_id: string;
+  secret_access_key: string;
+  region: string; (* e.g. "us-east-1"; preferably the same as used for EC2 *)
+  bucket: string;
+}
 
-   CreateBatchPrediction
-   GetBatchPrediction
-   DeleteBatchPrediction
-*)
+let make_url_and_headers
+    ~param
+    ~method_
+    ?(content_type = "text/plain")
+    ~path
+    ?action
+    ?(query_param = [])
+    ?(body = "")
+    () =
+  let region = param.region in
+  let host = sprintf "%s.%s.amazonaws.com" param.bucket service in
+  let query =
+    match action with
+    | None -> query_param
+    | Some s -> (s, []) :: query_param
+  in
+  let url = Uri.make ~scheme:"https" ~host ~path ~query () in
+  let headers =
+    Aws_call.make_headers
+      ~secret_access_key: param.secret_access_key
+      ~content_type
+      ~http_request_method: method_
+      ~host
+      ~region
+      ~service
+      ~path
+      ~request_payload: body
+      ()
+  in
+  url, headers
 
-let create_data_source_from_s3 () =
-  ()
+module Http = Util_http_client
+
+let handle_opt_http_response
+    parse_response_body
+    (resp_status, resp_headers, resp_body) =
+  match resp_status with
+  | `OK -> Some (parse_response_body resp_body)
+  | `Not_found -> None
+  | `Bad_request ->
+      logf `Error "Bad AWS S3 request. Response body: %s" resp_body;
+      failwith "Bad AWS S3 request"
+  | err ->
+      logf `Error "AWS S3 call failed with error %d: %s\n%!"
+        (Cohttp.Code.code_of_status err) resp_body;
+      Http_exn.service_unavailable "3rd-party service is unavailable"
+
+let handle_http_response parse_response_body ((_, _, body) as resp) =
+  match handle_opt_http_response parse_response_body resp with
+  | Some x -> x
+  | None ->
+      logf `Error "AWS S3 object not found. Response body: %s" body;
+      Http_exn.not_found "Resource not found"
+
+let get_object ~param ~path =
+  let url, headers = make_url_and_headers ~param ~method_:`GET ~path () in
+  Http.get ~headers url >>= fun resp ->
+  return (handle_http_response (fun s -> s) resp)
+
+let opt_get_object ~param ~path =
+  let url, headers = make_url_and_headers ~param ~method_:`GET ~path () in
+  Http.get ~headers url >>= fun resp ->
+  return (handle_opt_http_response (fun s -> s) resp)
+
+let put_object ~param ~path ~content_type contents =
+  let url, headers = make_url_and_headers ~param ~method_:`PUT ~path () in
+  Http.post ~headers url ~body:contents >>= fun resp ->
+  return (handle_http_response ignore resp)
+
+let delete_object ~param ~path contents =
+  let url, headers = make_url_and_headers ~param ~method_:`DELETE ~path () in
+  Http.delete ~headers url >>= fun resp ->
+  return (handle_http_response ignore resp)
